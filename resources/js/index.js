df025f41656a59d32196a5cc79ffcc43044ecac7
Ken Huang <102949773+KenHuangDev@users.noreply.github.com>
2022-04-09 16:44:49 -0700

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,14 @@
 /**
- * js
+ * index js
  */
+
+$(document).ready(function () {
+
+  /** scroll on button click */
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js--section-features').offset().top
+    }, 1000);
+  });
+  
+});
